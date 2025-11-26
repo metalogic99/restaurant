e5fc9f045bdb007b024678cd1a86891aec9dfb39
amritchapai <chapaiamrit@gmail.com>
2025-11-26 17:15:24 +0545

    feat: reservation model and create

---
+++ src/app/api/reservation/route.ts
@@ -0,0 +1,84 @@
+import { authenticate } from "@/middleware/authentication";
+import Reservation from "@/models/reservation.model";
+import Table from "@/models/tableModel";
+import connectDB from "@/utils/connectDB";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { name, time, date, peopleNos, phone, tableId, preOrdered } =
+      await req.json();
+    if (!name || !time || !date || !peopleNos || !phone || !tableId) {
+      return NextResponse.json(
+        {
+          error:
+            "Bad request, name, time, date, peopleNos, tableId and phone are required",
+        },
+        { status: 400 }
+      );
+    }
+    await connectDB();
+    const targetTime = new Date(`${date}T${time}:00`);
+    const fifteenMin = 15 * 60 * 1000;
+
+    const startTime = new Date(targetTime.getTime() - fifteenMin);
+    const endTime = new Date(targetTime.getTime() + fifteenMin);
+
+    const reservations = await Reservation.find({
+      date,
+      tableId,
+      time: { $gte: startTime, $lte: endTime },
+    });
+    console.log("reservation obtained is", reservations);
+    if (reservations.length > 0) {
+      return NextResponse.json(
+        {
+          error: "Reservation already exists for that time period",
+        },
+        { status: 409 }
+      );
+    }
+    const table = await Table.findById(tableId);
+    if (!table) {
+      return NextResponse.json(
+        {
+          error: "Table Id not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    const reservationCreated = new Reservation({
+      name,
+      time: targetTime.getTime(),
+      date,
+      peopleNos,
+      phone,
+      table,
+      preOrdered,
+    });
+    await reservationCreated.save();
+    return NextResponse.json(
+      { success: true, reservationCreated },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error creating reservation:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    await authenticate(req, ["admin"]);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Failed to reservations" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/models/reservation.model.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const ReservationSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    time: { type: String, required: true },
+    date: { type: Date, required: true },
+    peopleNos: { type: Number, required: true },
+    phone: { type: String, required: true },
+    preOrdered: { type: Boolean, default: true },
+    table: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Table",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Reservation = mongoose.model("Reservation", ReservationSchema);
+export default Reservation;
